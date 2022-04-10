cc1253897c87418af4518f08150b51e8c0881104
Vvvlad <lisenkovlad777@gmail.com>
2022-04-10 16:09:08 +0300

    a

---
+++ test_Git/Header.h
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void mission()
+{
+	const int size = 10;
+	int arr[size]{};
+	for (int i = 0; i < size; i++)
+	{
+
+	}
+}
--- test_Git/main.cpp
+++ test_Git/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include "Header.h"
 using namespace std;
 
 int main()
 {
-
+	const int size = 10;
+	setlocale(LC_ALL, "rus");
+	cout << "\tЗадание 1\n";
+	mission;
+	cout << "\n\tЗадание 2\n";
+	mission_1;
+	cout << "\n\tЗадание 3\n";
+	cout << "Числа: ";
+	mission_2;
 }

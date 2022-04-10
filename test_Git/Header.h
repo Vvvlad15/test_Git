4596406bb64f8bf80f5869a4bd07398a06d060d6
Vvvlad <lisenkovlad777@gmail.com>
2022-04-10 16:16:24 +0300

    222

--- test_Git/Header.h
+++ test_Git/Header.h
@@ -4,9 +4,18 @@ using namespace std;
 void mission()
 {
 	const int size = 10;
-	int arr[size]{};
+	int arr[size]{},;
 	for (int i = 0; i < size; i++)
 	{
+		arr[i] = rand() % 10;
+	}
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
 
+	for (int i = 0; i < size; i++)
+	{
+		
 	}
 }
